c4c880dc9dcc26ed6d77dcb9687b4ce4846d0af2
Kurt Kiefer <kekiefer@gmail.com>
2019-11-02 21:51:15 -0700

    HACK: reduce LIBUVC_NUM_TRANSFER_BUFS to work around failures on MacOS

--- include/libuvc/libuvc_internal.h
+++ include/libuvc/libuvc_internal.h
@@ -215,7 +215,7 @@ typedef struct uvc_device_info {
   We could/should change this to allow reduce it to, say, 5 by default
   and then allow the user to change the number of buffers as required.
  */
-#define LIBUVC_NUM_TRANSFER_BUFS 100
+#define LIBUVC_NUM_TRANSFER_BUFS 8
 
 #define LIBUVC_XFER_BUF_SIZE	( 16 * 1024 * 1024 )
 #define LIBUVC_XFER_META_BUF_SIZE ( 4 * 1024 )
